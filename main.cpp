b6765c3a3c0e6bc737be990d5fc3dbd05290001e
andre <andreasth005@gmail.com>
2025-05-17 03:05:21 +0300

    implemented try catch for the board constructor and made display prittier

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@ void printCell(Cell cell);
 
 int main()
 {
-    std::cout << "Hello world\n";
+    try
+    {
+        Board b = Board(8);
+        displayBoard(b);
+    }
+    catch(const invalid_argument& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-    Board b = Board(10);
-    displayBoard(b);
 }
 
 void printCell(Cell cell)
@@ -29,17 +35,51 @@ void printCell(Cell cell)
     }
 }
 
+// A is 0 and Z is 25
+char numToLetter(int n)
+{
+    if(n >= 0 || n <= 25){
+        return static_cast<char>('A' + n);
+    }
+    else{
+        throw invalid_argument("Input must be between 0 and 25");
+    }
+}
+
 void displayBoard(Board board)
 {
     int size = board.getSize();
 
-    for (int y = 0; y < size; y++)
+    cout << "    ";
+    for (int i = 0; i < size; i++)
     {
+        cout << numToLetter(i) << " ";
+    }
+    cout << endl;
+
+    for (int y = 0; y < size; y++)
+    { 
+        if(y >= 9){
+            cout << y+1 << "  ";
+        }
+        else{
+            cout << " " << y+1 << "  ";
+        }
+
         for (int x = 0; x < size; x++)
         {
             printCell(board.getCell(x, y));
             cout << " ";
         }
+        cout << "  " << y + 1;
+
         cout << endl;
     }
+
+    cout << "    ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << numToLetter(i) << " ";
+    }
+    cout << endl;
 }
