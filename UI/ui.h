5bdebb4196301ddf4a81ebcc7c1a3b4817628713
andre <andreasth005@gmail.com>
2025-05-18 18:49:49 +0300

    moved UI functions to the UI header file to make main.cpp cleaner

--- UI/ui.h
+++ UI/ui.h
@@ -4,4 +4,68 @@
 #include "Cell.h"
 #include "Board.h"
 
+void printCell(Cell cell)
+{
+    if (!cell.isGuessed()){
+        cout << 'O';
+        return;
+    }
+
+    if (cell.isHit()){
+        cout << 'X';
+    }
+    else{
+        cout << '-';
+    }
+}
+
+// A is 0 and Z is 25
+char numToLetter(int n)
+{
+    if(n >= 0 || n <= 25){
+        return static_cast<char>('A' + n);
+    }
+    else{
+        throw invalid_argument("Input must be between 0 and 25");
+    }
+}
+
+void displayBoard(Board board)
+{
+    int size = board.getSize();
+
+    cout << "    ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << numToLetter(i) << " ";
+    }
+    cout << endl;
+
+    for (int y = 0; y < size; y++)
+    { 
+        if(y >= 9){
+            cout << y+1 << "  ";
+        }
+        else{
+            cout << " " << y+1 << "  ";
+        }
+
+        for (int x = 0; x < size; x++)
+        {
+            printCell(board.getCell(x, y));
+            cout << " ";
+        }
+        cout << "  " << y + 1;
+
+        cout << endl;
+    }
+
+    cout << "    ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << numToLetter(i) << " ";
+    }
+    cout << endl;
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -8,38 +8,14 @@ void printCell(Cell cell);
 
 int main()
 {
-    std::cout << "Hello world\n";
-
-    Board b = Board(10);
-    displayBoard(b);
-}
-
-void printCell(Cell cell)
-{
-    if (!cell.isGuessed()){
-        cout << 'O';
-        return;
-    }
-
-    if (cell.isHit()){
-        cout << 'X';
-    }
-    else{
-        cout << '-';
-    }
-}
-
-void displayBoard(Board board)
-{
-    int size = board.getSize();
-
-    for (int y = 0; y < size; y++)
+    try
     {
-        for (int x = 0; x < size; x++)
-        {
-            printCell(board.getCell(x, y));
-            cout << " ";
-        }
-        cout << endl;
+        Board b = Board(8);
+        displayBoard(b);
     }
+    catch(const invalid_argument& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
 }
