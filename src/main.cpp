9ae60d29f2fafd391873164a08f56159f7d48caa
andre <andreasth005@gmail.com>
2025-05-20 15:43:26 +0300

    moved tests to seperate test file

--- src/main.cpp
+++ src/main.cpp
@@ -14,25 +14,4 @@ int main()
 
     Board& board = Controller::getBoard();
 
-    //PRINT BOARD WITH BORDER
-/*     for (int i = 0; i < b.getSize() + 2; i++)
-    {
-
-        for (Cell c : b1[i])
-        {
-            if (c.getType() == Cell::CellType::SEA){
-                cout << "O";
-            }
-            else if(c.getType() == Cell::CellType::BOAT){
-                cout << "A";
-            }
-            else{
-                cout << "X";
-            }
-            cout << " ";
-        }
-        cout << endl;
-    } */
-
-
 }
---
+++ tests/test1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include <logic/controller.h>
+#include <model/board.h>
+#include <model/cell.h>
+#include <util/utils.h>
+
+int main()
+{
+    Controller::initBoard(8);
+
+    Board& board = Controller::getBoard();
+
+    //PRINT BOARD WITH BORDER
+/*     for (int i = 0; i < b.getSize() + 2; i++)
+    {
+
+        for (Cell c : b1[i])
+        {
+            if (c.getType() == Cell::CellType::SEA){
+                cout << "O";
+            }
+            else if(c.getType() == Cell::CellType::BOAT){
+                cout << "A";
+            }
+            else{
+                cout << "X";
+            }
+            cout << " ";
+        }
+        cout << endl;
+    } */
+}
