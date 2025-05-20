cf7bce2d0c2215e30585b693bd6e589b1bd80f0b
andre <andreasth005@gmail.com>
2025-05-20 15:34:42 +0300

    finished basics of the Controller interface

---
+++ src/Logic/Controller.cpp
@@ -0,0 +1,41 @@
+#include <stdexcept>
+
+#include <logic/controller.h>
+#include <model/Cell.h>
+#include <model/board.h>
+
+namespace Controler{
+
+    static Board* boardInstance = nullptr;
+
+    void initBoard(int size)
+    {
+        if (boardInstance == nullptr){
+            boardInstance = new Board(size);
+        }
+    }
+
+    Board& getBoard()
+    {
+        if (boardInstance == nullptr){
+            throw std::runtime_error("Board not initialized.");
+        }
+        else{
+            return *boardInstance;
+        }
+    }
+
+    void guess(int x, int y)
+    {
+        Board& board = getBoard();
+
+        Cell& c = board.getCell(x, y);
+
+        c.setGuessed(true);
+        
+        //change cell state in the case of a hit
+        if (c.getType() == Cell::CellType::BOAT){
+            c.setHit(true);
+        }
+    }
+}
