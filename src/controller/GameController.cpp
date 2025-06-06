a389d6943b4c4483dad97ba0ed7e94c01d5bd6da
andre <andreasth005@gmail.com>
2025-06-06 11:25:01 +0300

    created the getters and the constructor

--- src/controller/GameController.cpp
+++ src/controller/GameController.cpp
@@ -4,3 +4,14 @@
 #include <board.h>
 #include <player.h>
 
+Controller::GameController::GameController(unsigned int boardSize) : model(boardSize), view() {}
+
+GameModel& Controller::GameController::getModel()
+{
+    return model;
+}
+
+GameView& Controller::GameController::getView()
+{
+    return view;
+}
