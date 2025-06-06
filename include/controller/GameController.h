e8f18a4b05a95184750e19c983d7918064530f9f
andre <andreasth005@gmail.com>
2025-06-06 11:17:51 +0300

    cleaned up the includes, moved Turn enum to the model class and made getters

--- include/controller/GameController.h
+++ include/controller/GameController.h
@@ -1,20 +1,23 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include <board.h>
-#include <player.h>
-
 #include <GameModel.h>
 #include <GameView.h>
 
-class GameController{
-public:
-    enum class Turn {PLAYER, AI};
-private:
-    GameModel model;
-    GameView view;
-public:
+namespace Controller
+{
+    class GameController{
+    private:
+        GameModel model;
+        GameView view;
+    public:
 
-    Turn currentTurn;
-};
+        GameController(unsigned int boardSize);
+
+        GameModel& getModel();
+        GameView& getView();
+
+    };
+}
+
 #endif
