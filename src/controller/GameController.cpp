7f754893e05dbe8c25aafc46a4ca9bb6dfaa0ecf
andreas <andreasth005@gmail.com>
2025-07-20 19:32:54 +0300

    initialized the buttons to callback the handleGuessButton function which was also defined here

--- src/controller/GameController.cpp
+++ src/controller/GameController.cpp
@@ -1,10 +1,45 @@
 #include <stdexcept>
 
+#include <iostream>
+
 #include <GameController.h>
 #include <board.h>
 #include <player.h>
 
-Controller::GameController::GameController(unsigned int boardSize) : model(boardSize), view() {}
+struct CallBackData{
+    Controller::GameController* controller;
+    Coord* coord;
+
+    CallBackData(Controller::GameController* c, Coord* _c) : controller(c), coord(_c) {}
+};
+
+void Controller::GameController::handleGuessButton(Fl_Widget* w, void* data)
+{
+    CallBackData* d = static_cast<CallBackData*>(data);
+    Controller::GameController* c = d->controller;
+    Coord* coords = d->coord;
+
+    if (c->model.getTurn() == GameModel::Turn::PLAYER1){
+        c->model.makeGuess(*coords);
+    }   
+
+}
+
+Controller::GameController::GameController(unsigned int boardSize, int wWidth, int wHeight) 
+    : model(boardSize), view(wWidth, wHeight, boardSize) 
+{
+    //initializing board button functions
+    for (unsigned int i = 0; i < boardSize; i++)
+    {
+        for (unsigned int j = 0; j < boardSize; j++)
+        {
+            CallBackData* d = new CallBackData(this, new Coord(j, i));
+            view.getOpponentBoard()[i][j]->callback(Controller::GameController::handleGuessButton, d);
+        }
+        
+    }
+    
+}
 
 GameModel& Controller::GameController::getModel()
 {
@@ -14,4 +49,10 @@ GameModel& Controller::GameController::getModel()
 GameView& Controller::GameController::getView()
 {
     return view;
+}
+
+void Controller::GameController::run()
+{
+    view.show();
+    Fl::run();
 }
