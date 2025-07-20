4e726e9d5a82be481aa42d4d7700126703a530b1
andreas <andreasth005@gmail.com>
2025-07-20 19:28:36 +0300

    makeGuess now switches the current turn after each call

--- src/model/GameModel.cpp
+++ src/model/GameModel.cpp
@@ -1,13 +1,19 @@
 
 #include <GameModel.h>
 
-GameModel::GameModel(unsigned int boardSize) : player1(boardSize), player2(boardSize) {}
+#include <iostream>
 
-void GameModel::makeGuess(coord c)
+GameModel::GameModel(unsigned int boardSize) : player1(boardSize), player2(boardSize), currentTurn(GameModel::Turn::PLAYER1) {}
+
+void GameModel::makeGuess(Coord c)
 {
     Player& target = (currentTurn == GameModel::Turn::PLAYER1) ? player2 : player1;
 
     target.applyGuess(c);
+
+    std::cout << "player" << ((currentTurn == GameModel::Turn::PLAYER1) ? "1 " : "2 ") << "guessed" << c.x << " " << c.y << std::endl;
+
+    currentTurn = (currentTurn == GameModel::Turn::PLAYER1) ? GameModel::Turn::PLAYER2 : GameModel::Turn::PLAYER1;
 }
 
 Player& GameModel::Player1()
@@ -17,4 +23,9 @@ Player& GameModel::Player1()
 Player& GameModel::Player2()
 {
     return player2;
+}
+
+GameModel::Turn GameModel::getTurn()
+{
+    return currentTurn;
 }
