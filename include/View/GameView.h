22710ff0c4442c48e3db317c15a190cee2ec358f
andreas <andreasth005@gmail.com>
2025-07-20 19:34:48 +0300

    created functions to create the window and the boards and 2d vector arrays to store pointers to the corresponding buttons for each board

--- include/View/GameView.h
+++ include/View/GameView.h
@@ -1,12 +1,29 @@
 #ifndef VIEW_H
 #define VIEW_H
 
+#include <vector>
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+
 class GameView{
 private:
+    Fl_Window *window;
+    std::vector<std::vector<Fl_Button*>> playerboard; //player1 board
+    std::vector<std::vector<Fl_Button*>> opponentBoard; //player2's board where player1 guesses.
 
+    //UI creations helpers
+    void createWindow(int width, int height);
+    void createBoards(unsigned int boardSize);
 public:
 
-    GameView();
+    GameView(int width, int height, unsigned int boardSize);
+
+    std::vector<std::vector<Fl_Button*>>& getPlayerBoard();
+    std::vector<std::vector<Fl_Button*>>& getOpponentBoard();
+
+    void show();
 
     void updateBoard();
 
